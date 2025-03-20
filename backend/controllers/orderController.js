0c696320bc927109c58d4c102c015cc7302c95f3
Mohamed Afraar <afraararm@gmail.com>
2025-03-20 17:13:25 +0530

    Implement createOrder API to handle order creation and calculate total amount

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,21 @@
-exports.createOrder = (req, res, next) => {
+const orderModel = require("../models/orderModel");
+
+//Create Order API - /api/v1/orders
+exports.createOrder = async (req, res, next) => {
+  const cartItems = req.body;
+  const amount = Number(
+    cartItems.reduce((acc, item) => acc + item.product.price * item.qty, 0)
+  ).toFixed(2);
+  const status = "pending";
+
+  const order = await orderModel.create({
+    cartItems,
+    amount,
+    status,
+  });
+
   res.json({
     success: true,
-    message: "order works!",
+    order,
   });
 };
