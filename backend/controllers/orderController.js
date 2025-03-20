ec108d0b95e4f559bc3216158dc2e8566d327e76
Mohamed Afraar <afraararm@gmail.com>
2025-03-20 18:31:15 +0530

    Refactor createOrder API to simplify order creation logic and update endpoint path

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,18 +1,13 @@
 const orderModel = require("../models/orderModel");
 
-//Create Order API - /api/v1/orders
+//Create Order - /api/v1/order
 exports.createOrder = async (req, res, next) => {
   const cartItems = req.body;
   const amount = Number(
     cartItems.reduce((acc, item) => acc + item.product.price * item.qty, 0)
   ).toFixed(2);
   const status = "pending";
-
-  const order = await orderModel.create({
-    cartItems,
-    amount,
-    status,
-  });
+  const order = await orderModel.create({ cartItems, amount, status });
 
   res.json({
     success: true,
