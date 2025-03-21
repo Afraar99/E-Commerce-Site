0cd496ca7e5607eafbef26ce92b33d7ae5944f77
Mohamed Afraar <afraararm@gmail.com>
2025-03-21 21:58:50 +0530

    Add Header and Footer components, and update App structure

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,39 +3,6 @@ import { Fragment } from "react";
 export default function Home() {
   return (
     <Fragment>
-      <nav className="navbar row">
-        <div className="col-12 col-md-3">
-          <div className="navbar-brand">
-            <img width="150px" src="/frontend/public/images/products/logo.png" />
-          </div>
-        </div>
-
-        <div className="col-12 col-md-6 mt-2 mt-md-0">
-          <div className="input-group">
-            <input
-              type="text"
-              id="search_field"
-              className="form-control"
-              placeholder="Enter Product Name ..."
-            />
-            <div className="input-group-append">
-              <button id="search_btn" className="btn">
-                <i className="fa fa-search" aria-hidden="true"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-          <span id="cart" className="ml-3">
-            Cart
-          </span>
-          <span className="ml-1" id="cart_count">
-            2
-          </span>
-        </div>
-      </nav>
-
       <h1 id="products_heading">Latest Products</h1>
 
       <section id="products" className="container mt-5">
@@ -184,12 +151,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-
-      <footer className="py-1 bg-dark">
-        <p className="text-center text-white mt-1">
-          JVLcart - 2023-2024, All Rights Reserved
-        </p>
-      </footer>
     </Fragment>
   );
 }
