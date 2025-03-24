830b68387c8a475f0275e753e99d0e61adb3d1b7
Mohamed Afraar <afraararm@gmail.com>
2025-03-24 19:32:31 +0530

    Implement routing in App component and update ProductCard to use Link for navigation

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function ProductCard({ product }) {
   return (
     <div className="col-sm-12 col-md-6 col-lg-3 my-3">
@@ -5,19 +7,24 @@ export default function ProductCard({ product }) {
         <img className="card-img-top mx-auto" src={product.images[0].image} />
         <div className="card-body d-flex flex-column">
           <h5 className="card-title">
-            <a href="">
-            {product.name}
-            </a>
+            <Link to={"/product/" + product.id}>{product.name}</Link>
           </h5>
           <div className="ratings mt-auto">
             <div className="rating-outer">
-              <div className="rating-inner" style={{width: `${product.ratings/5*100}%`}}></div>
+              <div
+                className="rating-inner"
+                style={{ width: `${(product.ratings / 5) * 100}%` }}
+              ></div>
             </div>
           </div>
           <p className="card-text">${product.price}</p>
-          <a href="#" id="view_btn" className="btn btn-block">
+          <Link
+            to={"/product/" + product.id}
+            id="view_btn"
+            className="btn btn-block"
+          >
             View Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
