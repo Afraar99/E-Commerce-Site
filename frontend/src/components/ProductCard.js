9d07a86c1cc45f34634cdf0506be1f141896e4aa
Mohamed Afraar <afraararm@gmail.com>
2025-03-24 19:18:21 +0530

    Update ProductCard and Home components to display dynamic product data

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,13 +1,12 @@
-export default function ProductCard() {
+export default function ProductCard({ product }) {
   return (
     <div className="col-sm-12 col-md-6 col-lg-3 my-3">
       <div className="card p-3 rounded">
-        <img className="card-img-top mx-auto" src="/images/products/1.jpg" />
+        <img className="card-img-top mx-auto" src={product.images[0].image} />
         <div className="card-body d-flex flex-column">
           <h5 className="card-title">
             <a href="">
-              OPPO F21s Pro 5G (Dawnlight Gold, 8GB RAM, 128 Storage) with No
-              Cost EMI/Additional Exchange Offers
+            {product.name}
             </a>
           </h5>
           <div className="ratings mt-auto">
@@ -15,7 +14,7 @@ export default function ProductCard() {
               <div className="rating-inner"></div>
             </div>
           </div>
-          <p className="card-text">$245.67</p>
+          <p className="card-text">${product.price}</p>
           <a href="#" id="view_btn" className="btn btn-block">
             View Details
           </a>
