5cb6087411b4d30626392f8e0d2af90d9d64999c
Mohamed Afraar <afraararm@gmail.com>
2025-04-03 11:44:14 +0530

    Add react-toastify for notifications and update ProductDetail component to handle cart functionality

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -7,7 +7,7 @@ export default function ProductCard({ product }) {
         <img className="card-img-top mx-auto" src={product.images[0].image} />
         <div className="card-body d-flex flex-column">
           <h5 className="card-title">
-            <Link to={"/product/" + product.id}>{product.name}</Link>
+            <Link to={"/product/" + product._id}>{product.name}</Link>
           </h5>
           <div className="ratings mt-auto">
             <div className="rating-outer">
@@ -19,7 +19,7 @@ export default function ProductCard({ product }) {
           </div>
           <p className="card-text">${product.price}</p>
           <Link
-            to={"/product/" + product.id}
+            to={"/product/" + product._id}
             id="view_btn"
             className="btn btn-block"
           >
