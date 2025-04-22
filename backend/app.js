ca68a1dcfb2e2483bd96795b44e1e13ac4813eef
Mohamed Afraar <afraararm@gmail.com>
2025-04-22 12:05:46 +0530

    Add authentication and cookie parsing middleware to the backend

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,24 @@ const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectDatabase = require("./config/connectDatabase");
 dotenv.config({ path: path.join(__dirname, "config", "config.env") });
 
 const products = require("./routes/product");
 const orders = require("./routes/order");
+const auth = require("./routes/auth");
 
 connectDatabase();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
+
+// API Routes
 app.use("/api/v1/", products);
 app.use("/api/v1/", orders);
+app.use("/api/v1/", auth);
 
 app.listen(process.env.PORT, () => {
   console.log(
